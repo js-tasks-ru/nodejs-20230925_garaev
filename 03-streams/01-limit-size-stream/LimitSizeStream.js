172b9d819920922dc6f93a14ce104521c60939d9
garaev-vadim-m <kawsvadik453@mail.ru>
2023-10-08 17:25:12 +0300

    03 - Stream with data transfer limit

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,48 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  _default = Number(0);
+
   constructor(options) {
     super(options);
+    this.limit = options.limit;
   }
 
-  _transform(chunk, encoding, callback) {
+  bufferEncode(chunk, encoding) {
+    if(encoding === 'buffer') {
+      return chunk;
+    }
+    return Buffer.from(chunk, encoding);
+  };
 
+  get accum() {
+    return this._default;
+  }
+  set accum(value) {
+    this._default = value;
+  }
+
+  isLength(value) {
+    return this.limit >= value;
+  };
+
+  processData(chunk, encoding) {
+    const bufferChunk = this.bufferEncode(chunk, encoding);
+    this.accum += bufferChunk.length;
+
+    if (!this.isLength(this.accum)) {
+      throw new LimitExceededError();
+    }
+    return chunk;
+  };
+
+  _transform(chunk, encoding, callback) {
+    try {
+      const processedChunk = this.processData(chunk, encoding)
+      callback(null, processedChunk);
+    } catch (error) {
+      callback(error, null);
+    }
   }
 }
 
