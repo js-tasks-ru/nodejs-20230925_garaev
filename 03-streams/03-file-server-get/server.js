d2bcc10a10c90566fcb77c883ecb8603573d2c80
garaev-vadim-m <kawsvadik453@mail.ru>
2023-10-15 16:03:10 +0300

    03 / 03 - File server - file upload

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,24 +1,46 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
+server.on('request', (request, response) => {
+  const url = new URL(request.url, `http://${request.headers.host}`);
   const pathname = url.pathname.slice(1);
-
+  response.setHeader('Content-Type', 'text/html; charset=utf-8');
   const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
-    case 'GET':
 
+  let eventStream = null;
+
+  switch (request.method) {
+    case 'GET':
+      if (pathname.split('/').length > 1) {
+        response.statusCode = 400;
+        return response.end('Ошибка:  такого файла не существует');
+      }
+      eventStream = fs.createReadStream(filepath);
+      eventStream.on('error', (err)=> {
+        response.statusCode = 404;
+        response.end('Ошибка:  такого файла не существует');
+      });
+      eventStream.on('open', ()=> {
+        response.statusCode = 200;
+        eventStream.pipe(response);
+      });
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      response.statusCode = 501;
+      response.end('Not implemented');
   }
+
+  request.on('aborted', ()=> {
+    if (eventStream) {
+      eventStream.destroy();
+    }
+  });
 });
 
 module.exports = server;
