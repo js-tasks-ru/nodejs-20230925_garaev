bb40cea3b5eb30ea5ba29795a2c18bf61eca4b6d
garaev-vadim-m <kawsvadik453@mail.ru>
2023-10-15 17:22:54 +0300

    03 / 04 - File server - write file

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,23 +1,73 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('node:fs');
+
+const LimitSizeStream = require('./LimitSizeStream.js');
+
+const ONE_MEGABYTE = 1048576;
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
+const unlinkFileWhenAborted = (stream, pathFile)=> {
+  +
+  stream.destroy();
+  fs.unlink(pathFile, (err)=> {
+    if (err) throw err;
+  });
+};
+
+server.on('request', (request, response) => {
+  const url = new URL(request.url, `http://${request.headers.host}`);
   const pathname = url.pathname.slice(1);
+  response.setHeader('Content-Type', 'text/html; charset=utf-8');
 
   const filepath = path.join(__dirname, 'files', pathname);
 
-  switch (req.method) {
+  let limitedStream = null;
+
+  switch (request.method) {
     case 'POST':
+      if (pathname.split('/').length > 1) {
+        response.statusCode = 400;
+        response.end('Ошибка: неверный путь до файла');
+        return;
+      }
+      limitedStream = new LimitSizeStream({
+        limit: 1024 * 1024,
+      });
+      fs.stat(filepath, function(error, stat) {
+        if (!error) {
+          response.statusCode = 409;
+          response.end('Файл уже создан на диске');
+          return;
+        }
+        const writeFileStream = fs.createWriteStream(filepath);
+
+        writeFileStream.on('pipe', ()=> {
+          request.on('aborted', unlinkFileWhenAborted.bind(null, writeFileStream, filepath));
+        });
+
+        writeFileStream.on('finish', ()=> {
+          request.off('aborted', unlinkFileWhenAborted);
+          response.statusCode = 201;
+          response.end('Файл успешно записан на диск');
+        });
+
+        limitedStream.on('error', (err)=> {
+          unlinkFileWhenAborted(writeFileStream, filepath);
+          response.statusCode = 413;
+          response.end(err.message);
+        });
+
+        request.pipe(limitedStream).pipe(writeFileStream);
+      });
 
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      response.statusCode = 501;
+      response.end('Not implemented');
   }
 });
 
